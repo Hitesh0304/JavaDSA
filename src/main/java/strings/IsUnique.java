6f2d07cfe1cb530aa214efd73c74410fdcedfe43
Hitesh0304 <31934805+Hitesh0304@users.noreply.github.com>
2025-07-18 15:49:36 -0400

    optimized isUnique solution using hashSet

--- src/main/java/strings/IsUnique.java
+++ src/main/java/strings/IsUnique.java
@@ -4,18 +4,37 @@ package strings;
 Question - Implement an algo to determine if a string has all unique characters. What if you cannot use additional data structures?
  */
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class IsUnique {
-    public static void main (String[] args) {
-        String string = "Hitesh";
-        System.out.println("String has all unique characters: " + isUniqueString(string));
+    public static void main(String[] args) {
+        String first = "Hitesh";
+        String last = "Moudgil";
+
+        System.out.printf("String - \"%s\" has all unique characters: %s%n", first, isUniqueString(first));
+        System.out.printf("String - \"%s\" has all unique characters: %s ", last, isUniqueString(last));
     }
 
     public static boolean isUniqueString(String str) {
-        for(int i = 0; i < str.length(); i++) {
-            for(int j = i+1; j < str.length(); j++) {
-                if(str.toLowerCase().charAt(i) == str.toLowerCase().charAt(j)) {
-                    return false;
-                }
+
+        //time complexity is O(n^2)
+//        for(int i = 0; i < str.length(); i++) {
+//            for(int j = i+1; j < str.length(); j++) {
+//                if(str.toLowerCase().charAt(i) == str.toLowerCase().charAt(j)) {
+//                    return false;
+//                }
+//            }
+//        }
+//        return true;
+
+        //Using Set - time complexity O(n)
+        Set<Character> characterSet = new HashSet<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (!characterSet.contains(str.toLowerCase().charAt(i))) {
+                characterSet.add(str.toLowerCase().charAt(i));
+            } else {
+                return false;
             }
         }
         return true;
